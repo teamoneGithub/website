e5d61f6296be77a5f05bd7129716c1369e8eea4e
tkardach <tommy@kardach.com>
2017-07-16 20:37:55 -0700

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,6 +1,6 @@
 <?php
 	header('Access-Control-Allow-Origin: *');
-	$dbCon = mysqli_connect("athena.ecs.csus.edu","teamone_user","teamone_db","teamone");
+	$dbCon = mysqli_connect(/* Removed for public repository */);
 	if(mysqli_connect_errno()){
 		echo "Failed to connect: ".mysqli_connect_error();
 	}
